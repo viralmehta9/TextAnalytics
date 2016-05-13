a4223d2a63edec7d2b7ad55b3e2184da8fc417ca
Viral Mehta <viralmehta9@users.noreply.github.com>
2016-05-13 03:28:27 -0500

    Update code

--- LocationBasedTweets.py
+++ LocationBasedTweets.py
@@ -16,17 +16,20 @@ from TwitterSearch import TwitterSearchException
 try:
     tso = TwitterSearchOrder() # create a TwitterSearchOrder object
     tso.set_key
-    tso.set_keywords(['swamy39']) # let's define all words we would like to have a look for
-    tso.set_language('en') # we want to see German tweets only
+    tso.set_keywords(['trump']) # let's define all words we would like to have a look for
+    tso.set_language('en') # we want to see English tweets only
     tso.set_include_entities(False) # and don't give us all those entity information
     
     # it's about time to create a TwitterSearch object with our secret tokens
+    # tokens are modified and dummy values are shown.
     ts = TwitterSearch(
-        consumer_key = 'V3vtYV8UdvoQPfk0hkoA3hEI1',
-        consumer_secret = 'N9Gy4HQpn598ENjRSI71JCKUsh7ivG73YWR7kvwaBhzWHB8zmT',
-        access_token = '893216012-rXzWnM1PWf89YHF7wu8xrhMooBkDeEHhjW6AcMXq',
-        access_token_secret = 'ko7P2wsDwMiYZ24iyWdcQet5RJF8TgqCBERaYjKLEtjYo'
+        consumer_key = 'V3vV8UdvoQPfk0hkoA3hEI1',
+        consumer_secret = 'N9Gy4HQpn698ENjHAF71JCKUsh7egA73Y7H3kvwaHwfswzmT',
+        access_token = '89321hMoo60-rXz1PWf89YHxrBkDeEF8HhjcMXq',
+        access_token_secret = 'koF8gq7wMiYZ24iydcQetJF8TgqCBERaKLEtjYo'
+
      )
+     
 
     MAX_LIMIT = 50
     count =0    
@@ -38,55 +41,7 @@ try:
             break
 except TwitterSearchException as e: # take care of all those ugly errors if there are some
     print(e)
-
-
-
-
-
-
-
-
-'''
-from twython import Twython
-#from collections import Counter
-
-#import string
-
-api_key = "V3vtYV8UdvoQPfk0hkoA3hEI1"
-api_secret = "N9Gy4HQpn598ENjRSI71JCKUsh7ivG73YWR7kvwaBhzWHB8zmT"
-access_token = "893216012-rXzWnM1PWf89YHF7wu8xrhMooBkDeEHhjW6AcMXq"
-token_secret = "ko7P2wsDwMiYZ24iyWdcQet5RJF8TgqCBERaYjKLEtjYo"
-api = Twython(api_key, api_secret, access_token, token_secret)
-
-def get_tweets(twython_object, query, n):
-    result_generator = twython_object.cursor(twython_object.search, q = query)
-    return result_generator
     
-rg = get_tweets(api, '#trump',1)
-MAX_LIMIT = 1 #limit tweets to 10
-print (type(rg))
-tweets = []
-count = 0
-for r in rg:
-    #print(r['text'])
-    tweets.append(r)
-    count += 1
-    #print(r)
-    print(r['user']['location'])
-    if count == MAX_LIMIT:
-        break
-    #print(r)
     
-        
-tweetSentScore = []
-wordList = []
-documentList = []
-for tweet in tweets:
-    txt = tweet['text']
-    #txt = parse(txt)
-    documentList.append(txt)
-    words = txt.split()
-    for item in words:
-        wordList.append(item)
     
-'''  
+    
